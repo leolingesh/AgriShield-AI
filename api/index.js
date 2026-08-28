28e87b0f47db274bb7420b4c2c57fc115ccf0fba
LINGESH M <lingesh.m.cse29@princedrkvasudevan.com>
2026-08-28 12:30:28 +0530

    fix: diagnostic wrapper for Vercel serverless function

--- api/index.js
+++ api/index.js
@@ -1,3 +1,19 @@
-const app = require('../server/server');
+let app;
+try {
+  app = require('../server/server');
+} catch (err) {
+  console.error('Error loading Express app in api/index.js:', err);
+  const express = require('express');
+  app = express();
+  app.use(express.json());
+  app.all('*', (req, res) => {
+    res.status(500).json({
+      success: false,
+      message: 'Serverless initialization error',
+      error: err.message,
+      stack: err.stack
+    });
+  });
+}
 
 module.exports = app;
